c5a58cf0ac0afde751c9589ca1f32d1572a62c36
GrPaladino <paladino.graziano@gmail.com>
2024-02-06 16:03:54 +0100

    Feat: add bonus

--- js/main.js
+++ js/main.js
@@ -5,24 +5,36 @@ const app = createApp({
     return {
       message: "Hello Vue!",
       mails: [],
+      show: false,
     };
   },
 
   methods: {
     generateMail() {
-      axios
-        .get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then((response) => {
-          const mail = response.data.response;
-          this.mails.push(mail);
-        });
+      for (let i = 0; i < 10; i++) {
+        axios
+          .get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then((response) => {
+            const mail = response.data.response;
+            this.mails.push(mail);
+          });
+      }
+    },
+
+    showMails() {
+      this.show = true;
+
+      if (this.mails.length < 10) this.generateMail();
+    },
+
+    deleteMails() {
+      this.show = false;
+      this.mails.splice(0, 10);
     },
   },
 
   created() {
-    for (let i = 0; i < 10; i++) {
-      this.generateMail();
-    }
+    this.generateMail();
   },
 });
 app.mount("#root");
