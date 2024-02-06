d4415823d32f8ccf300523307322b1db6eff58a2
GrPaladino <paladino.graziano@gmail.com>
2024-02-06 15:16:17 +0100

    Feat: generated and printed on page 10 emails

--- js/main.js
+++ js/main.js
@@ -4,9 +4,25 @@ const app = createApp({
   data() {
     return {
       message: "Hello Vue!",
+      mails: [],
     };
   },
 
-  methods: {},
+  methods: {
+    generateMail() {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((response) => {
+          const mail = response.data.response;
+          this.mails.push(mail);
+        });
+    },
+  },
+
+  created() {
+    for (let i = 0; i < 10; i++) {
+      this.generateMail();
+    }
+  },
 });
 app.mount("#root");
